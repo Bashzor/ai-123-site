d664754ccf766f500a0278284f9b197032fb5248
Bashzor <jamesaddisonorr@gmail.com>
2026-01-19 09:51:27 -0500

    Update contact.js

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -1,34 +1,38 @@
-export async function onRequest({ request }) {
-  // CORS
-  if (request.method === "OPTIONS") {
-    return new Response(null, {
-      status: 204,
+export async function onRequestPost({ request }) {
+  try {
+    const data = await request.json().catch(() => ({}));
+
+    return new Response(JSON.stringify({
+      ok: true,
+      received: data
+    }), {
+      status: 200,
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type"
+        "Content-Type": "application/json"
+      }
+    });
+  } catch (err) {
+    return new Response(JSON.stringify({
+      ok: false,
+      error: err.message
+    }), {
+      status: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
       }
     });
   }
+}
 
-  if (request.method !== "POST") {
-    return new Response(JSON.stringify({ ok: false, error: "Method not allowed", method: request.method }), {
-      status: 405,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "content-type": "application/json"
-      }
-    });
-  }
-
-  // Echo back what we received
-  const body = await request.json().catch(() => ({}));
-  return new Response(JSON.stringify({ ok: true, received: body }), {
-    status: 200,
+export async function onRequestOptions() {
+  return new Response(null, {
+    status: 204,
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "content-type": "application/json"
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
     }
   });
 }
-
