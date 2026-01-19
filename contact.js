f88a3c88d45a323bc39d4cfc56955b778fea4269
Bashzor <jamesaddisonorr@gmail.com>
2026-01-19 09:52:44 -0500

    Update contact.js

--- contact.js
+++ contact.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     try {
-      const res = await fetch("/api/contact", {
+      const res = await fetch("/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
